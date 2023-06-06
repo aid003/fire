a7b3ad84e3215eb654b28d3e0ffc88e17ace175c
aid003 <teremochek859@gmail.com>
2023-06-07 00:55:51 +0300

    v4

--- src/app/libary/ModalWindow.js
+++ src/app/libary/ModalWindow.js
@@ -1,20 +1,24 @@
 import Image from "next/image";
 import React from "react";
 import styles from "./libary.module.css";
+import { ImCancelCircle } from "react-icons/im";
 
 const ModalWindow = ({ img, title, text }) => {
   return (
-    <div className={styles.openCard}>
-      <Image
-        alt=""
-        className={styles.imgModal}
-        width={450}
-        height={300}
-        src={img}
-      />
-      <h3 className={styles.headingModal}>{title}</h3>
-      <div className={styles.textContainerModal}>{text}</div>
-    </div>
+    <>
+      <ImCancelCircle className={styles.cancelImg}/>
+      <div className={styles.openCard}>
+        <Image
+          alt=""
+          className={styles.imgModal}
+          width={450}
+          height={300}
+          src={img}
+        />
+        <h3 className={styles.headingModal}>{title}</h3>
+        <div className={styles.textContainerModal}>{text}</div>
+      </div>
+    </>
   );
 };
 

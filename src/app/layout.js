088f539e61597a01a93d55116ce41507890e7b44
aid003 <teremochek859@gmail.com>
2023-06-06 13:17:36 +0300

    v3

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
+      <link rel="icon" href="/src/app/favicon.ico" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
   );
